e5ea928af77f21a292915f9173a376702a97464e
Ibrahim-Muhammad13 <ibrahimmuhammad13720@gmail.com>
2023-04-20 10:35:33 +0200

    change dir

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    //
+
+    public function home(){
+        $userType = \Auth::user()->userType;
+        if($userType == 1){
+            return view('admin.index');
+        }
+        return view('dashboard');
+    }
+}
